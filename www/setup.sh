d062f9403494e8e5682d006b9d14d16b64c82e54
poldrack <poldrack@gmail.com>
2017-07-26 08:43:59 -0700

    initial

---
+++ www/setup.sh
@@ -0,0 +1,6 @@
+cd /var/www/html/app
+#git clone https://github.com/psych10/psych10-shinyapps.git
+#cd /var/www/html/app/psych10-shinyapps; git pull
+
+chown -R www-data:www-data /var/www/html/app/psych10-shinyapps
+
