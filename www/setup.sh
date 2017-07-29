ca2bcbcd3ff94630a2ffb7b611d2cc3a881d1468
poldrack <poldrack@gmail.com>
2017-07-29 14:23:34 -0700

    fix setup

--- www/setup.sh
+++ www/setup.sh
@@ -1,5 +1,5 @@
 cd /var/www/html/app
-#git clone https://github.com/psych10/psych10-shinyapps.git
+git clone https://github.com/psych10/psych10-shinyapps.git
 #cd /var/www/html/app/psych10-shinyapps; git pull
 
 chown -R www-data:www-data /var/www/html/app/psych10-shinyapps
